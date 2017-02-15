65226e6e55a840e20dc6edb3b93c8a31e22eb848
DmitrySamsonov <dimasamsonov7@gmail.com>
2017-02-15 12:21:13 +0300

    try to sort strings using Arrays.sort(); - not work correctly

--- SortingDataWebApp/src/com/acdlabs/sotringdata/webapp/gwt/client/SortingDataWebApp.java
+++ SortingDataWebApp/src/com/acdlabs/sotringdata/webapp/gwt/client/SortingDataWebApp.java
@@ -1,11 +1,16 @@
 package com.acdlabs.sotringdata.webapp.gwt.client;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 
 
 public class SortingDataWebApp implements EntryPoint {
@@ -15,6 +20,10 @@ public class SortingDataWebApp implements EntryPoint {
 	private FlexTable displayResultTable = new FlexTable();
 	private Button processButton = new Button("Process");
 	
+	private TextArea inputDataTextarea2 = new TextArea();
+	
+	private ArrayList<String> stocks = new ArrayList<String>();
+	
 	
 	/*
 	 * Entry point
@@ -23,7 +32,15 @@ public class SortingDataWebApp implements EntryPoint {
 		
 		drawElements();
 		
-		getDataFromTextArea();
+		// Listen for mouse events on the Process button.
+		processButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				getDataFromTextArea();
+			
+			}
+		});
+		
+		
 		  
 	    sortData();
 		  
@@ -34,10 +51,14 @@ public class SortingDataWebApp implements EntryPoint {
 	private void drawElements() {
 		
 		// Let's make an 80x50 text area to go along with the other two.
-		inputDataTextarea.setCharacterWidth(80);
+		inputDataTextarea.setCharacterWidth(40);
 		inputDataTextarea.setVisibleLines(50);
+		
+			inputDataTextarea2.setCharacterWidth(40);
+			inputDataTextarea2.setVisibleLines(50);
 						  
 		horizontalPanel.add(inputDataTextarea);
+		horizontalPanel.add(inputDataTextarea2);
 		horizontalPanel.add(processButton);
 		horizontalPanel.add(displayResultTable);
 
@@ -49,7 +70,21 @@ public class SortingDataWebApp implements EntryPoint {
 	}
 
 	private void getDataFromTextArea() {
+		final String lines = inputDataTextarea.getText();
+		inputDataTextarea.setFocus(true);
 		
+		
+		String[] words = lines.split("\n");
+		
+		Arrays.sort(words);
+		
+		String result = "";
+		for(String elem : words)
+		{
+			result += elem + "\n";
+		}
+		inputDataTextarea2.setText(result);	
+	    
 	}
 
 	private void sortData() {
