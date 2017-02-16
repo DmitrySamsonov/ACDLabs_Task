bec22344ee5c8427110379a3986cb56a492e0599
DmitrySamsonov <dimasamsonov7@gmail.com>
2017-02-17 01:23:40 +0300

    save inputData after Refresh Page

--- SortingDataWebApp/src/com/acdlabs/sotringdata/webapp/gwt/client/SortingDataWebApp.java
+++ SortingDataWebApp/src/com/acdlabs/sotringdata/webapp/gwt/client/SortingDataWebApp.java
@@ -1,25 +1,26 @@
 package com.acdlabs.sotringdata.webapp.gwt.client;
 
-import java.util.*;
-
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Grid;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-
+import com.google.gwt.storage.client.Storage;
 
 public class SortingDataWebApp implements EntryPoint {
 
 	private HorizontalPanel horizontalPanel = new HorizontalPanel();
+	private VerticalPanel verticalPanel = new VerticalPanel();
 	private TextArea inputDataTextarea = new TextArea();
 	private Button processButton = new Button("Process");
+	private Button initButton = new Button("Set some data");
 	private Grid grid = new Grid();
 	
-	
+	private Storage stockStore = null;
 	
 	/* 
 	 * Entry point
@@ -28,28 +29,65 @@ public class SortingDataWebApp implements EntryPoint {
 		
 		drawElements();
 		
-		// initialization		
-		inputDataTextarea.setText(
-				"-2.2	2	3	4	329	2" + "\n" + 
-				"2.2	12345q	69	-afg" + "\n" +
-				"2.2	12345q	69	-asdf" + "\n" +
-				"-22	1234234	asdfasf	asdgas" + "\n" +
-				"-22	11	abc" + "\n" +
-				"-22	-3	4" + "\n" +
-				"" + "\n" +
-				"-1.1" + "\n" +
-				"" + "\n" +
-				"qqqq	1.1");
 		
+		stockStore = Storage.getLocalStorageIfSupported();
+		if (stockStore != null){
+		  for (int i = 0; i < stockStore.getLength(); i++){
+		    String key = stockStore.key(i);
+		    
+		    String tmp = inputDataTextarea.getText();
+			tmp += stockStore.getItem(key);
+
+			inputDataTextarea.setText(tmp);
+		  }
+		}
+	
 		
 		// Move cursor focus to the input box.
 		inputDataTextarea.setFocus(true);
 				
-	
+		
+		
+		// Listen for mouse events on the Set some data button.
+		initButton.addClickHandler(new ClickHandler() {
+			public void onClick(ClickEvent event) {
+				
+				// initialization		
+				inputDataTextarea.setText(
+						"-2.2	2	3	4	329	2" + "\n" + 
+						"2.2	12345q	69	-afg" + "\n" +
+						"2.2	12345q	69	-asdf" + "\n" +
+						"-22	1234234	asdfasf	asdgas" + "\n" +
+						"-22	11	abc" + "\n" +
+						"-22	-3	4" + "\n" +
+						"" + "\n" +
+						"-1.1" + "\n" +
+						"" + "\n" +
+						"qqqq	1.1");
+				
+			}
+		});
+		
+		
 		
 		// Listen for mouse events on the Process button.
 		processButton.addClickHandler(new ClickHandler() {
 			public void onClick(ClickEvent event) {
+				
+				// clear Storage
+				stockStore = Storage.getLocalStorageIfSupported();
+				 if (stockStore !=null) {
+				      stockStore.clear();
+				 }
+				 
+				// Save new inputData to Storage  
+				final String symbol = inputDataTextarea.getText();
+				stockStore = Storage.getLocalStorageIfSupported();
+				if (stockStore != null) {
+				  int numStocks = stockStore.getLength();
+				  stockStore.setItem("Stock."+numStocks, symbol);
+				}
+				
 				
 				String inputData = inputDataTextarea.getText();
 				
@@ -76,7 +114,7 @@ public class SortingDataWebApp implements EntryPoint {
 			for(int column = 0; column < grid.getColumnCount() ;column++)
 				grid.clearCell(row, column);
 
-		
+		// Insert data to Grid
 		for(int row = 0; row < lines.length ; row++){
 			
 			String[] words = lines[row].split("\t"); 
@@ -99,13 +137,16 @@ public class SortingDataWebApp implements EntryPoint {
 		
 		inputDataTextarea.addStyleName("textarea");
 		grid.addStyleName("grid");
-		processButton.addStyleName("processButton");
-		
+		processButton.addStyleName("button");
+		initButton.addStyleName("button");
+
+		horizontalPanel.add(initButton);
 		horizontalPanel.add(inputDataTextarea);
 		horizontalPanel.add(processButton);
 		horizontalPanel.add(grid);
+		
 
-		RootPanel.get("sortingwidget").add(horizontalPanel);		
+		RootPanel.get("sortingwidget").add(horizontalPanel);
 	}
 	
 }
